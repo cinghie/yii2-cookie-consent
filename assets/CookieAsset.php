29804a05bee6b9e0009426e782b15dc2d276dd79
Albert Borsos <albertborsos@me.com>
2016-02-28 01:19:15 +0100

    fix phpcs

--- assets/CookieAsset.php
+++ assets/CookieAsset.php
@@ -1,35 +1,35 @@
 <?php
 
 /**
-* @copyright Copyright &copy; Gogodigital Srls
-* @company Gogodigital Srls - Wide ICT Solutions 
-* @website http://www.gogodigital.it
-* @github https://github.com/cinghie/yii2-cookie-consent
-* @license GNU GENERAL PUBLIC LICENSE VERSION 3
-* @package yii2-cookie-consent
-* @version 1.4.2
-*/
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-cookie-consent
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-cookie-consent
+ * @version 1.4.2
+ */
 
 namespace cinghie\cookieconsent\assets;
 
 class CookieAsset extends \yii\web\AssetBundle
 {
-	/**
+    /**
      * @inherit
      */
-	public $sourcePath = '@bower/cookieconsent2/build/';
-	
-	/**
+    public $sourcePath = '@bower/cookieconsent2/build/';
+
+    /**
      * @inherit
      */
-	public $js=[
-		'cookieconsent.min.js',
-	];
-	
-	/**
+    public $js = [
+        'cookieconsent.min.js',
+    ];
+
+    /**
      * @inherit
      */
-	public $depends = [
+    public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
         'yii\bootstrap\BootstrapPluginAsset',
--- widgets/CookieWidget.php
+++ widgets/CookieWidget.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
-* @copyright Copyright &copy; Gogodigital Srls
-* @company Gogodigital Srls - Wide ICT Solutions
-* @website http://www.gogodigital.it
-* @github https://github.com/cinghie/yii2-cookie-consent
-* @license GNU GENERAL PUBLIC LICENSE VERSION 3
-* @package yii2-cookie-consent
-* @version 1.4.2
-*/
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-cookie-consent
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-cookie-consent
+ * @version 1.4.2
+ */
 
 namespace cinghie\cookieconsent\widgets;
 
@@ -16,70 +16,71 @@ use Yii;
 
 class CookieWidget extends \yii\base\Widget
 {
-  public $message;
-  public $dismiss;
-  public $learnMore;
-  public $link;
-  public $theme;
-  public $container;
-  public $path;
-  public $domain;
-  public $expiryDays;
+    public $message;
+    public $dismiss;
+    public $learnMore;
+    public $link;
+    public $theme;
+    public $container;
+    public $path;
+    public $domain;
+    public $expiryDays;
 
-  public function init(){
-	  parent::init();
+    public function init()
+    {
+        parent::init();
 
-	  // Default Value
-	  if(!$this->message) {
-	  	  $this->message = 'This website uses cookies to ensure you get the best experience on our website.';
-	  }
+        // Default Value
+        if (!$this->message) {
+            $this->message = 'This website uses cookies to ensure you get the best experience on our website.';
+        }
 
-	  if(!$this->dismiss) {
-	  	  $this->dismiss = 'Got It!';
-	  }
+        if (!$this->dismiss) {
+            $this->dismiss = 'Got It!';
+        }
 
-	  if(!$this->learnMore) {
-	  	  $this->learnMore = 'More info';
-	  }
+        if (!$this->learnMore) {
+            $this->learnMore = 'More info';
+        }
 
-	  if(!$this->link) {
-	  	  $this->link = null;
-	  }
+        if (!$this->link) {
+            $this->link = null;
+        }
 
-	  if(!$this->theme) {
-	  	  $this->theme = 'dark-bottom';
-	  }
+        if (!$this->theme) {
+            $this->theme = 'dark-bottom';
+        }
 
-	  if(!$this->container) {
-	  	  $this->container = null;
-	  }
-	  
-	  if(!$this->path) {
-	  	  $this->path = "/";
-	  }
+        if (!$this->container) {
+            $this->container = null;
+        }
 
-	  if(!$this->domain) {
-	  	  $this->domain	= "";
-	  }
+        if (!$this->path) {
+            $this->path = "/";
+        }
 
-	  if(!$this->expiryDays) {
-	  	  $this->expiryDays	= 365;
-	  }
-  }
+        if (!$this->domain) {
+            $this->domain = "";
+        }
 
-  public function run($params = [])
-  {
-      return $this->render('cookieWidget',[
-		  'message'    => $this->message,
-		  'dismiss'    => $this->dismiss,
-	  	  'learnMore'  => $this->learnMore,
-          'link'       => $this->link,
-          'theme'      => $this->theme,
-		  'container'  => $this->container,
-		  'path'       => $this->path,
-		  'domain'     => $this->domain,
-		  'expiryDays' => $this->expiryDays
-      ]);
-  }
+        if (!$this->expiryDays) {
+            $this->expiryDays = 365;
+        }
+    }
+
+    public function run($params = [])
+    {
+        return $this->render('cookieWidget', [
+            'message' => $this->message,
+            'dismiss' => $this->dismiss,
+            'learnMore' => $this->learnMore,
+            'link' => $this->link,
+            'theme' => $this->theme,
+            'container' => $this->container,
+            'path' => $this->path,
+            'domain' => $this->domain,
+            'expiryDays' => $this->expiryDays
+        ]);
+    }
 
 }
