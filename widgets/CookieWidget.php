a77e5b2d5132b54ab64b4bfbb4c8b53680d78703
Giandomenico Olini <giando.olini@gogodigital.it>
2016-03-03 18:45:47 +0100

    Update version

--- widgets/CookieWidget.php
+++ widgets/CookieWidget.php
@@ -7,7 +7,7 @@
 * @github https://github.com/cinghie/yii2-cookie-consent
 * @license GNU GENERAL PUBLIC LICENSE VERSION 3
 * @package yii2-cookie-consent
-* @version 1.4.2
+* @version 1.4.3
 */
 
 namespace cinghie\cookieconsent\widgets;
