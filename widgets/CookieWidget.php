83025c404b62b83b8643c5737e08af8968c0d082
Giandomenico Olini <giando.olini@gogodigital.it>
2016-03-03 18:49:21 +0100

    fixes

--- widgets/CookieWidget.php
+++ widgets/CookieWidget.php
@@ -26,62 +26,45 @@ class CookieWidget extends \yii\base\Widget
   public $domain;
   public $expiryDays;
   
-  public function init(){
+  public function init()
+  {
 	  parent::init();
 	  
 	  // Default Value
 	  if(!$this->message) {
 	  	  $this->message = 'This website uses cookies to ensure you get the best experience on our website.';
-	  } else {
-  	  	  $this->message = $this->message;
 	  }
 	  
 	  if(!$this->dismiss) {
 	  	  $this->dismiss = 'Got It!';
-	  } else {
-  	  	  $this->dismiss = $this->dismiss;
 	  }
 	  
 	  if(!$this->learnMore) {
 	  	  $this->learnMore = 'More info';
-	  } else {
-  	  	  $this->learnMore = $this->learnMore;
 	  }
 	  
 	  if(!$this->link) {
 	  	  $this->link = null;
-	  } else {
-  	  	  $this->link = $this->link;
 	  }
 	  
 	  if(!$this->theme) {
 	  	  $this->theme = 'dark-bottom';
-	  } else {
-  	  	  $this->theme = $this->theme;
 	  }
 	  
 	  if(!$this->container) {
 	  	  $this->container = null;
-	  } else {
-  	  	  $this->container = $this->container;
 	  }
 	  
 	  if(!$this->path) {
 	  	  $this->path = "/";
-	  } else {
-  	  	  $this->path = $this->path;
 	  }
 	  
 	  if(!$this->domain) {
 	  	  $this->domain	= "";
-	  } else {
-  	  	  $this->domain = $this->domain;
 	  }
 	  
 	  if(!$this->expiryDays) {
 	  	  $this->expiryDays	= 365;
-	  } else {
-  	  	  $this->expiryDays = $this->expiryDays;
 	  }
   }
   
