22318eb95fd6fff5c2ab76344fb7a6badab19ea7
Giandomenico Olini <giando.olini@gogodigital.it>
2016-11-13 18:41:50 +0100

    Update CookieAsset.php

--- assets/CookieAsset.php
+++ assets/CookieAsset.php
@@ -7,7 +7,7 @@
 * @github https://github.com/cinghie/yii2-cookie-consent
 * @license GNU GENERAL PUBLIC LICENSE VERSION 3
 * @package yii2-cookie-consent
-* @version 1.4.3
+* @version 1.4.4
 */
 
 namespace cinghie\cookieconsent\assets;
